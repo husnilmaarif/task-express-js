82875dd76541b7a9b52d0f5f040c469b50c99e8e
husnil maarif <husnilmaarif.hm@gmail.com>
2022-02-24 17:38:32 +0800

    task express js

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-console.log("halo dunia");
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+// using ejs
+app.set("view engine", "ejs");
+
+// third party middleware
+app.use(expressLayouts);
+app.use(morgan("dev"));
+
+// build in middleware
+app.use(express.static("public"));
+
+// application level middleware
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    layout: "layouts/main-layout",
+    title: "Home Page",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "About Page",
+  });
+});
+
+app.get("/list", (req, res) => {
+  const siswa = [
+    {
+      nama: "Muhammad Husnil",
+      asal: "Makassar",
+    },
+    {
+      nama: "Muhammad Isana",
+      asal: "Jakarta",
+    },
+    {
+      nama: "Muhammad Fadel",
+      asal: "Surabaya",
+    },
+  ];
+  res.render("list", {
+    layout: "layouts/main-layout",
+    title: "List Page",
+    students: "Students at Eduwork",
+    siswa: siswa,
+  });
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("Not Found 404");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
