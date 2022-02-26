9442bf8fbd7f242cc57e8bb628fb45c9e5396f9f
husnil maarif <husnilmaarif.hm@gmail.com>
2022-02-26 09:56:11 +0800

    task express

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // using ejs
 app.set("view engine", "ejs");
